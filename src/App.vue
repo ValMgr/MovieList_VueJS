046fcbaa6cc5a2efccd41874c52a44011c22fdd9
Magry Valentin <valentin.magry@etu.u-bordeaux.fr>
2020-12-20 16:06:22 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <header class="navbar">
+      <section class="navbar-section">
+        <div id="title">
+          <h1>VueJs - DAWIN</h1>
+        </div>
+      </section>
+      <section class="navbar-section">
+        <div id="nav">
+         <router-link to="/">Home</router-link> |
+         <router-link to="/movies">Movies</router-link>
+        </div>
+      </section>
+    </header>
+
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+}
+
+body{
+  background-color: rgb(33, 37, 41);
+}
+
+#nav {
+  padding: 30px;
+}
+
+h1{
+  color: #42b983;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #ffffff;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.navbar{
+  position: fixed;
+  justify-content: center;
+  z-index: 10;
+  width: 100%;
+  padding: 10px 0;
+  background-color: rgb(33, 37, 41);
+  box-shadow: 0 0.25rem 1rem #42b98370;
+  align-items: stretch;
+  display: flex;
+  display: -ms-flexbox;
+  -ms-flex-align: stretch;
+  -ms-flex-pack: justify;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  top: 0;
+} 
+
+.navbar section{
+  align-self: center;
+}
+
+.navbar section:first-child {
+  float: left;
+  margin-left: 50px;
+}
+
+.navbar section:last-child {
+  float: right;
+  margin-right: 50px;
+}
+
+.container:first-of-type{
+  margin-top: 15vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+
 
 Vue.config.productionTip = false
 
+window.global_data = {
+  movies: [
+    {title: "13 Hours", year: "2016", productor: "Michael Bay", short: "Le 11 septembre 2012, à Benghazi..."},
+    {title: "Titanic", year: "1998", productor: "James Cameron", short: "Le Titanic est un paquebot transatlantique..."},
+    {title: "Interstellar", year: "2014", productor: "Christopher Nolan", short: "Dans un futur proche, la Terre est de moins en moins accueillante..."},
+  ]
+}
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
